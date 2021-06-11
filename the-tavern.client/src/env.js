02d473203e638430c46a35a3e1da8de0f40384cc
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-06-10 20:13:52 -0600

    tavern build push

--- the-tavern.client/src/env.js
+++ the-tavern.client/src/env.js
@@ -1,5 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'samwise.us.auth0.com'
+export const domain = 'dev-tkj14bxx.us.auth0.com'
 export const audience = 'https://the-tavern.com'
-export const clientId = 'vfHtDsBSt6M1y32FFdyOcv7LTV4qh9Ol'
+export const clientId = '8gJ5ARBDHmgIDEnWrCwGxmlOmuw99lWu'
+
+
